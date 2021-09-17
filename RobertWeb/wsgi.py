74f675293b963613e8a43933a637ca621322d4f7
Robert Arifulin <robertarif2015@gmail.com>
2021-09-17 09:07:29 +0300

    FirstCommit

---
+++ RobertWeb/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for RobertWeb project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RobertWeb.settings')
+
+application = get_wsgi_application()
