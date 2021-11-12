a81f8f0a9b3048e26040ac4c537b65d117585b74
Robert Arifulin <robertarif2015@gmail.com>
2021-11-12 09:38:36 +0300

    login and signup skeleton

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,15 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from PIL import Image
 
+User._meta.get_field('email')._unique = True
 
 # Create your models here.
 class Avatar(models.Model):
-    avatar = models.ImageField()
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True)
+    avatar = models.ImageField(upload_to="images", validators=[FileExtensionValidator(['png'])])
 
     def __str__(self):
         return f'{self.avatar.name}'
--- game/models.py
+++ game/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
+User._meta.get_field('email')._unique = True
 # Create your models here.
 
 class Leaderboard(models.Model):
